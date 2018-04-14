0d938597018f291a68c056b55983a27c81c4531e
brotzky <brotzky@gmail.com>
2018-04-14 15:17:31 -0700

    Updating server logic

--- src/server/middleware/proxy.js
+++ src/server/middleware/proxy.js
@@ -7,75 +7,38 @@ const isDev = process.env.NODE_ENV === 'development';
  * is executing. So we are creating our own --env.dev value.
  */
 const env = {
-  org: process.argv[2] || 'demo',
-  env: process.argv[3] || 'dev',
+  env: process.argv[2] || 'dev',
 };
 
-if (!process.argv[2] && isDev) {
-  throw new Error(
-    'You must provide the organization and enviroment: > yarn start <organization> <env>',
-  );
-}
-
 const target = 'localhost:8080';
-const BOC = [
-  '1stchoice',
-  'aldergrove',
-  'gffg',
-  'mountainview',
-  'lakeland',
-  'sunshinecoast',
-  'synergy',
-  'yncu',
-];
 
 function buildEnvOnRequest(req) {
   const host = req.get('host');
   const subdomain = host.split('.')[0].split('-');
-  const subdomainOrg = subdomain[0];
-  const subdomainEnv = subdomain[1];
+  const domainEnv = subdomain[1];
 
   // Workaround for localhost
   if (host.includes('localhost')) {
     return {
-      url,
-      subdomainOrg: env.org,
-      subdomainEnv: env.env,
+      env: env.env,
     };
   }
 
   return {
-    url,
-    subdomainOrg,
-    subdomainEnv,
+    env: domainEnv,
   };
 }
 
 export function buildDynamicTarget(req) {
-  const { url, subdomainOrg, subdomainEnv } = buildEnvOnRequest(req);
+  const { env } = buildEnvOnRequest(req);
 
-  if (req.get('host') === 'loans-gac.meridiancu.ca') {
-    return 'https://admin-api-dot-grow-meridian-prod.appspot.com';
-  }
-
-  // A workaround for BOC API
-  if (BOC.some(bocGroup => subdomainOrg === bocGroup)) {
-    return `https://admin-api-dot-grow-boc-${subdomainEnv}.appspot.com`;
-  }
-
-  return `https://admin-api-dot-grow-${subdomainOrg}-${subdomainEnv}.appspot.com`;
+  return `https://api-${env}.appspot.com`;
 }
 
 export function buildDynamicHeaders(proxyReq, req, res) {
-  const { url, subdomainOrg, subdomainEnv } = buildEnvOnRequest(req);
-  let forwardHost;
+  const { env } = buildEnvOnRequest(req);
 
-  // Production workaround
-  if (req.get('host') === 'loans-gac.meridiancu.ca') {
-    forwardHost = 'loans-gac.meridiancu.ca';
-  }
-
-  forwardHost = `${subdomainOrg}-${subdomainEnv}-gac.poweredbygrow.com`;
+  const forwardHost = `${env}.example.com`;
 
   proxyReq.setHeader('X-Forwarded-Host', forwardHost);
 }
--- src/server/utils/startup.js
+++ src/server/utils/startup.js
@@ -1,12 +1,13 @@
 const startup = () => {
   console.log('║');
   console.log('║');
-  console.log('║    Welcome to the Grow Admin Console');
+  console.log('║    Product starter');
   console.log('║');
   console.log(`║    🚀   ==>  App is running in ${process.env.NODE_ENV} mode`);
   console.log(
-    `║    🎯   ==>  API target https://${process.argv[2]}-${process.argv[3] ||
-      'dev'}-gac.appspot.com`,
+    `║    🎯   ==>  API target https://${
+      process.argv[2]
+    } || 'dev'}.appspot.com`,
   );
   console.log(
     `║    💻   ==>  Local server running on http://localhost:${
@@ -14,14 +15,6 @@ const startup = () => {
     }/`,
   );
   console.log('║    🕐   ==>  Please wait for compilation to finish.');
-  console.log('║');
-  console.log('║');
-  console.log(
-    '║    Gitlab: https://gitlab.com/poweredbygrow/software/grow-admin-console',
-  );
-  console.log(
-    '║    Jira: https://poweredbygrow.atlassian.net/secure/RapidBoard.jspa?rapidView=81&projectKey=GAC',
-  );
   console.log('║');
   console.log('║');
 };
